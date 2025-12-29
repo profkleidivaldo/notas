9f955f27d01b42ea1bbe4b0e8789c133948266a4
profkleidivaldo <profkleidivaldo@gmail.com>
2025-12-29 12:12:53 -0400

    Delete 5A/5A.js

--- 5A/5A.js
+++
@@ -1,9 +0,0 @@
-// Arquivo gerado automaticamente
-// Turma: 5A
-
-window.DADOS_TURMA = [
-  {
-    "ra": "12345",
-    "dados": "U2FsdGVkX1925ivYQeUnEZ3B89UE5zHzNkSW52wrEtRvUYQgfoiJVsiZUdqC6Q/JZGE4lDM0OxckS38TkH4bIZB0ncfblahgKWXrZne0n1dUXfVDndrjYcWZbWLqHAyRCKG9h3mjL0YVinJDhrh7JE7GVC7EwXFcdjBdLGcYLTb629uy7MRRar83kQh5FI3KR+qpd1K4WKCNthywRxJF9tE+EcxeNMk3mE/HODbu+jHqwUZbnNbWXbKNjxOIoLBB"
-  }
-];
