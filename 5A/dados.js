2e65e021c588b8bbeac6970279d8f7a52539a2bc
profkleidivaldo <profkleidivaldo@gmail.com>
2026-01-02 14:10:36 -0400

    Add files via upload

---
+++ 5A/dados.js
@@ -0,0 +1,122 @@
+window.DADOS_TURMA=[
+  {
+    "nome": "Allice Victória Lacerda",
+    "dados": "U2FsdGVkX1+qnfVV8e+niHf7Fvu7hOWmbhzCkvRoDTdmr9OC32kfc0DKFUwp9Et124ouQKgh17DHRHBt8B/RJ387NeNE3AC3GIy0+7b74JzYhZtlZwcU6CKbPeu5fJCuN5SNp9kkFBQVlPFdu6quPxMvndGLl/vnd6fUoalVTzRJZbf9C6r32Y9sZVd3m5pa7H5+5pimoskfkAo/A/+iAQm8jH5kj+WH3Uj19LyO8wOXc7YEg35jPsf8tl1W/FBs"
+  },
+  {
+    "nome": "Cristian Dantas Santos",
+    "dados": "U2FsdGVkX1/Reb3eVacOWEczb1RTkzpXN3Td4jM48aElMyUdLkFVdqTyfhrqzcwPwwVyiEoCPmBpZWtwZwp3lPImw+4qd7Wkp8lwIby8urCAkw5tM9dFnHOT9JsrPV2q+gJ/MmLHiAv+vgBO39Wxu4oVWIx6hTzrySW4kpXLo7mBVQ0THNCKmdDMW5QoX6UA5BLwLF+HW3/hwFW5QXMpbrlKHEf2Z2gx4lcqnsKbieKekaKGGTvAlQn4JKJJlXiX"
+  },
+  {
+    "nome": "Emylly Bianca da Silva Corrêa",
+    "dados": "U2FsdGVkX1+kIsOlq2DpWa7o3rh3kbCEtVHXTITwh8fUTiwLZjS8oMNjJrdKKEpCn/gpt6y90HFnJisD+NkT7bBirjftVzoLxnFnkO0WOjmnramNSeKleCKNuO3dZqaCPThCmgzKN6dz6SioONy2SQNrEL+1DPu6k5f/sXls1GMcZy2kAEkCrhq9v5qvYh22SWIpHqBp1S8pN2fm3cVYJh2Ya2M4T0ov5lLejQYwtJ11E4GhiWU3t0D11pMd1VUM"
+  },
+  {
+    "nome": "Esther Emanuelly Rueda Franco",
+    "dados": "U2FsdGVkX199DBEHews1oDHxgQBxmuHVIm+Ds7vVOmYsSxzpRnQj2nfaimrYn9uVabW9e+/BG5P2ISjGfCJiCwz8cdy3Vr7uHVWfjNF7qanqTD1yScPCp7SaLQldqUG0QvgYkMnNajF/2mrKG80spnQ3TujPvdN1B3oeKqYHCjospDwZQRoloXPWpXDOJ9KreaDRjHgsrbtqeXimRB39rEaQeiSpEYJGiO6Ef3RBPKFgVhth/bLErTrzdHlWKW9I"
+  },
+  {
+    "nome": "Eyshila Sophia Soares de Almeida",
+    "dados": "U2FsdGVkX18iVo1PSI24JKJnwNWojIVcqpv2HIrwuY+hvtrNCkGYIgdiak5Vnnve8A1bTTpxNXlGSwPa5EuYT+l6Pe7ptTfvowZvJf9HNcSz3HG45e+GOQTkQ/eTKrkOhjNKdWtwFxcrFzaFBzrDTPjcaRr/zfh0jynwsQhXVllJsNvJxZ93Trso5HERyEE6Meu+t/DQKBJFQctSTeT70lbEEabjVpVDu8AOyNEj+1hh6ExTmzwc2R/3yTolUnKf"
+  },
+  {
+    "nome": "Eloah Isabelly Ferrim Farage",
+    "dados": "U2FsdGVkX1+T+dgMCkTVz6b+PZ+HUcmu/wpyzKRgCy+QG7Mr6pqIzshA1GKzJtCfmYWCw174ZPNetWJ+tGoidN1IRrzn+ca50sTSxWFr8i5hqrV7fmIyGnaFzqSQU6a68Hr1pPaC11pvW0UI3GL8Qwf5JuvVsyDPNTfHd2S8EYr4axD8jN+VqISnexb5afwDZijSQt09GHr7qIeckCJJkUlFQSZHDeYqoAM4qxZ84tzRVxjpsSty6kSwxUAl8G07"
+  },
+  {
+    "nome": "Fábia Pietra Oliveira Januário Garcia",
+    "dados": "U2FsdGVkX1+oR2fkiwVjXztjAFXbXVvMjXsV2uWxKKid5pFgg2MvdD37+3QTO2LPdk2k5sbJ1XaeXpG8p8wDkXR1CjL/HFdNv41+XRPVq6nlZmshIcIXg79psBagv/Xdz0Bvy51gwHzeAyAH9L5oP6GShr4eDlZN4d0Zs+nJ1KbH1Efs1AFBj6SC6XxP3NfBHy/xCXjdrTLs2yGvWCoYnlviba6NE3jw/40m/1pkCQvvcS5c6BxBGrpLExVmYGSWrV5ZnfzPBPOCJ3Y/Ky7yvQ=="
+  },
+  {
+    "nome": "Gabriel Oliveira dos Santos",
+    "dados": "U2FsdGVkX191pCZ/KAhqn0p81d7aKGEMv5IO8T7RzT3XJWOYGTQblZRB2VJ9+gLFfQJHYVvx4wdoBoufw9VbLOgnTXi0ircKWcH+ZwifBK1rUDb1dscHHXqCKAyT0QorV1PooFH/NEhnKzUv2sDKwS0Z+dUYUfKgaL7fWu7VE1cuwk/tGE6h5MvrsGKZeftuDGwdt1CJT2GZ6oCLq/hQ29XwX4Eg5923RFkXpMF6hP1X1+0vXWyhmZS8gdyRNJZz"
+  },
+  {
+    "nome": "Geovane Chaves Ferreira",
+    "dados": "U2FsdGVkX18O3Zq5PNJ4ci89VTulSi2gZSR+Av3WzOsdgYN1Tu4jg5peY2eS+fP9CWP3C1QqAchUW7UcFhL4BHe0le8DSPYrHDG5SHwX8b++fk45KaxSJ9HJOzBd9KHmQt9SBMWC1nmIGX+9LsRUlpILD4vkEVs/hHlzVDry+sfY+V2Tsvz2w4nAFwSSsBI2vClkKYWi/8q4ifWd6+PVznd53Mzd5PUZnObTVq7Jait2VhULi+zXYJ6NgLdfwdap"
+  },
+  {
+    "nome": "Gustavo Lopes de Souza Guimarães",
+    "dados": "U2FsdGVkX1/+DYp0Du/Q7X2SCEFqdfGv+BVkTVdVD1OU454Zr/UvDtv2MCm4H+reXbe9EEme0N7F6UCdY0UgHXBpZMtgEwfItarNPvr7qUwkr7WWxQxr+wk42oLX4Vowba7xK3pblgFYn/5rAMz6+65tg5PejP4CFjloX9i06qoStzFmNXU+sxH45nj2EMMOJe2KlFhisw9f5+GjZNbaWKDRFh0PMChI25gDR4S3vLRFvPMBsZBNcY34Tp1eIlWF"
+  },
+  {
+    "nome": "Isabella Vitória da Silva Rueda",
+    "dados": "U2FsdGVkX19XZwYH3u/8A2l1aizz1YgyNee0eatFlIRS/ccNXl3Dfb8Q26kBTc8URGqeM3qZobmDyQOE36ow10btAgwuvizdFIKeukyQsjAA9MImuwvAkgf2futv7vGVM/OusXAJfeDK66z6sMj6pPERXc0v1sE+RxtPXAeoLyqbjKlV91EYMMIIsBHUvNL1pSa9KppgNyEqdkSfM2cBFz4JRMI2xiD/ycs52yboHNxcDq8YKFlAGlVD0tds3zSj"
+  },
+  {
+    "nome": "João Miguel Fernandes Silva",
+    "dados": "U2FsdGVkX19sz6Rp6poH4phkROq0sML+X9w/T2+/3R8fTKjq+NwAcxraWiEKJIL5a/QkSrdH588g2jc86k6Q7BgBecg++kslp9iNGbs3FF8/PM4W8izjwoIyNJZrvQNTFTykNX0W1m+75d5oMkEzQrCxN6ybfC09KaiHDqG8YKyeEe5Q+CtR7wj5aS/odjxmRG5elxxna8wMRy6r+eonnjbHxb4zWlatChNN1VnhBARQxydEp7o5S6lHh39pHDLc"
+  },
+  {
+    "nome": "Juliano Miguel Soares Rosa",
+    "dados": "U2FsdGVkX1+MaQ34ZaMm4mxY9LKDAOpOg9FHEMNhsOIYjhpKJxbKyOViKB3akjM/agsymNu0aqkZ6VdlAW169QZZPTsxRc3CBFG9b8JI9cKmWaVKPB4cz2WSAgxMnqMerHYhb/Gf0zY3zwhhf79Kn5uc9MrJrddMM2XawR7A37NFayhuPyBs4hEM5wt8TykAa8HM4VKw798l126icKtVewtuAdUNiYu47Uh7UIQY19AfHzHUVSi7dQyQTAEDS+gP"
+  },
+  {
+    "nome": "Laysla Sofia Carvalho dos Santos",
+    "dados": "U2FsdGVkX18rBgfj4leFVPMGt1JSQw7jazn3vUhvaGOr8qa/QTxTbbH3rtbxlwfuJorz0XC82HOqj1/yA6v6hhFryRShy6qT0M1+MNFg+Lfih0D8PGduCDaBCxRrfjIm7iWZ2UrrJGSsIH+kEml+rgSBn+GvaZMYcSNFb3np7UHTzrcyKKIw+Wdlxdcz+Bw1l4tJDGnqxMHkXl8BX+BjRB1ubvEs2WLXMzlzzbth9G6FwrIT1SEsXIdFtyjpTKkX"
+  },
+  {
+    "nome": "Luiz Felipe Honorio Moura",
+    "dados": "U2FsdGVkX1+0lE5zu20lmk+Zruplf7mLCSKOE4SKQF8yzocJodKw3BIC1+es6qceaeSP3fIHICHr49AXLetYFN23TNGhNZxP9z6/RVh3bfLYjDmPrxpWOASiJjurLA7htvUWjAtjF6VZMM8BNjCepfLQzJMx5pq6X0vnEbVEuy80RQLAYVkaTpvKr923a1UkYiOZuBweEIBXnPj/ZEzhRiUDuXjbxPUKZzdAaBQlxaEQYU7C1HrZwJJXCwahH7mv"
+  },
+  {
+    "nome": "Nícolas Gabriel da Silva Dias",
+    "dados": "U2FsdGVkX19uY8VOYFWKnw9snyBAZ723/NUyk5pTgQKrjfvmiwpFlopGKS8t8cmDIYhmOR27OhVmZMH8OUhlOxzZohTMUKoerCa+7TqBh3tM6PdYfJo7x9bcXKJraPgq/oWEWkvIl+bVKw31YFD1u2GFlB37VwS3nEBjH5CEuFkVfFMS3ZOu/wKDydp0KlPVlRvWfx7rFxSoftiEN1KiN2s/9x9ajXyRJDy3a5Zix6QunMvbbM8/MbAoezLdCN+n"
+  },
+  {
+    "nome": "Nycollas Evêncio de Lima",
+    "dados": "U2FsdGVkX1+f+ZV24N74qJf/EpRDcC8VisHCO3nGOR4vo7c+qEDSefEEBrCr5ghsCuN3XyO7iem3qmVjMN3deoaILQUSYM0hsr6C1kcgfZDGKmuFyDquEiERnAgicw5IzUBK2Cy2Zdsa2BwwlzeweecFgh/dUQILZJ7hCplwpZ2PBEnP4ZXROMh7poIzO5fESL8ScafGUF8lBEYW2osnOCDIooWaQkE/Csi4LHh8KRbAyy96AC2MZUUFxcp3J7PU"
+  },
+  {
+    "nome": "Rafael Gomes Camilo",
+    "dados": "U2FsdGVkX1/FKiWbC/pRU+Aj7/Iw8WG5aIHmrGZPqNAeT4jrox2wZlVdsV299VOzOt6rsnZrC57uFJnWRhOpMemQiVdkTaBGYajEeS8sS2eUGPwj30Kxk4FKfZWQkpoM94s9rFJZP0OzkUw1Ob6xjfN1SBi1TK00XkgZyAdcM2stZCD3T7AarbetzzWl0rE6/VVbQ1J7tNT2NVICslv/mZUp06hlhVEodmhiXjtibFg="
+  },
+  {
+    "nome": "Rosivaldo Mateus Cardoso",
+    "dados": "U2FsdGVkX1/6Bj7gpCzVh9Rb1a7JinkGzKqnIlxxKIUWN2Oc/sgtK8w4uw8No3Ly8XOm38Sr2kzejoEwjj0+A0kiZ0ZEuYclhCoe12Yvj7nzV0oaV8Nyq3q7453vLfI1LdOqlVMykuHVKIOqzPTCSsAg2NqdozMj9hBZw80ymY0TLOSXMkndJnCiVQWq0UKpTi+C1AhCeqQd8mTa9Rsg/k8WzcL00enxnWYbfx+hu1YnuqEn73ZdZncnlGOE9B68"
+  },
+  {
+    "nome": "Yanny Mariany de Lana Araujo",
+    "dados": "U2FsdGVkX1/mHlgT1VI2g6jtDsNE9hgIgnYOuKTTg+xfCwsdMCIPClPKszeWTESZQBvPtbRpq5uaUUWzWHq0nBU1EhpTn7CQNJm7CiNzise2vaXHW+dLsTPqTiAfgod50xbp8JerZLIp1PMZ7Ct1Mwf/QQ10/NiNChZOwDplOGRRa+hTEPF2ZEzDUAspfdcuPRy6WfGWt36QZf3oQtIis2wdg1jb+TJkGCpI3PNBkbqMJeO5jjvgib9X9kbpt3BX"
+  },
+  {
+    "nome": "Ysadora Tandara de Oliveira Brito",
+    "dados": "U2FsdGVkX1+GdoETneVrXMFmfxgkkWP3dd2JWdeQ97bhnAptcZ92f3VtIwdDittMQ76ieyE13dJN8wA/FlUaaFQ001IbG3vi9XeN0t0qATzBd//h/J7YkJhz+WkbKBsIpsDBeacNK+povuMZ1PmkUxKUHOxAXQFev6kQFPCrL19QbLi8g4gMXSzSw5gpbiDuYjX8AMZidwqeaW91QLEF9XoUldb52Mc40xHTZiH5nVxOvIXWzFcimsjlV/bNhGtv"
+  },
+  {
+    "nome": "Bianca Emanuelly oliveira gomes",
+    "dados": "U2FsdGVkX18DZw6IHIcE7Ga7f1Z7J5m9J+JlxgaPG7XQvxOoGcxju1MNyaUO/rD0kDbpmrX9MEt+EFQ4AesMf9b2HVzUZ3QqPhZczPla33shk2YkWhqpsBewgCjDfNN+kR/T7oBAu4wNdf5vVA4vlp1SUiymLIiJKXp6lZKX3HoOC4nKSDYZgszEG7lL1DeKo/od4ipahjD7gOie0SEL1sxtxhkBoO1y8tSZIDTK6M7CLxa5zpvufjQEmawSbWUC"
+  },
+  {
+    "nome": "Arthur Ferreira Castro",
+    "dados": "U2FsdGVkX18q/vU1G6cNtl8DYYgY3iZcNJRYlGdd6LQCxFDODnCN0/8aJiPnj3FnHqn4ykogiIxmxh3kaFtnrilsHSXEIbo4ZqAndhlZ6pVXbaqaRBTfAyL0MtTpVDwTPVB7/zklSgYkHB3y8aA7kMzOJ0kHaJ2TYl+CCu4jZ86KVz88EE+DIfmWKTn/2HeZMm1w4sFmTLsvxInZtQYtSjFEFNDqkb9NcNMYF7bhOccGRHGWVaua/4ZPGkvvAWN6"
+  },
+  {
+    "nome": "Willian Eduardo Suzarte Silva Barreto Jatoba ",
+    "dados": "U2FsdGVkX19F3XLlFJt/UxNNS6ApMcJ1SH0w3m7pShui35XjtUMufBCraP3K2zZX1dKyCyRa3V7VGCZSV3F/gjQfv2YYixRAe5JkHTgMGPkQAy+Z/+tRbCJ7xbnpOSDCm8eV0tmOwQr3m8CEyQKH0VsxVT5ZiSKAt0BwewU91hibLgnQTvDPumvkhcNPRG7AU8HLnS6rsHStouNRy71hHckqmccVOMlEHHQpHmjireZQf/bBscZ1qtx+0SwKAEP0OoKzDT8CzFLCzvz8/EdGdg=="
+  },
+  {
+    "nome": "Gabriella Hellen Brito Torres",
+    "dados": "U2FsdGVkX1+pb+E6WcSFd2oaAUyN7TrpYbQqVexoaSZS+HlAu4eFNs+PAHVRrcotPUN9bJmwTxpmgDeZZLSl/wV7KhtzEVv1VYau4/3wM3ksl3hRK5arWxS6+5xchhgz8sTjf+1ndSpQHbpHnvA6386jtMla8LhGi4fTcBMT5twtTf1TpqQQ19uBoVLQH8dbVaqQAWhdzjclzwBj9kgO1aakZ1wA43MRSTwQLWpm0csoprjw1DaciCwMT6Enc/PI"
+  },
+  {
+    "nome": "Valentina Ribeiro Dondoni",
+    "dados": "U2FsdGVkX19IHbI/u8mzwn9eGArWc4+KM8T4Hm0ul4y9xwHdaSoIMaK9bhNV1eJK5WeoL6cAv4fikycBGbWQdnj03n/FDwy9U40JNrY6mhrnUWSMq6oIc93RAC8vRNsgp5fGFKkbpbtkeO/yPBUqlf3pa7HUl/PsqCcV3xPmGl+GQDoIGz/aBBlyUMyWdYwWzgrqH1MiMfSDRTWNdlDGuP11h/bgNYsgH+1xqtSebVwre/ApGVtHNh+fiM7j+hYW"
+  },
+  {
+    "nome": "Maria gabriella helen Brito Torres",
+    "dados": "U2FsdGVkX19NfnXjXSac6RSEWYZbylrZMUVlhinttIYBTemCw2KDtmnmvIQwQBvttYsiGw345qaBuDg3hTAIXFZ1+iYfjiKYEILwWe4fdBDnlrj/sy45ehA74Vm8NMnzw39QJMi7VmhcP/763CuYovLM1Ddk8Aj4220uGA1Iv2e2tQuIR9I8ychI1iHNxT497lAr10m5tV8VOOcHc+vLch9sbMUanebbmlA7bBhCtPL0tTv4ygSZoV/FySKvap5X"
+  },
+  {
+    "nome": "aluno 28",
+    "dados": "U2FsdGVkX19X6KRqLAhObBYNOOReKmfAwN9f3z37JLBxOxJPd3w76FN9mhkGuMTTTZTFa5oIoCIKFc/TgUTfqUB+IzwqPrf+Nn7HpXEek92ZGWy70w36XKUJatPWDEEDPjh/HQ/dgZr61Z9XQLB0dIlpT/wKCkNEVzK9Mn3d0gzKYlIF7/udtsxgqvLE+CuI/XqpgelQPhmCXUbcmzMiFw=="
+  },
+  {
+    "nome": "aluno 29",
+    "dados": "U2FsdGVkX1/pYlL4fV/Opc6PH9GZg9xe4vhjr16uEKX+hC9Z7iWz8KOsMUNeH0iKT4+HD45unSuWcDmU0Go/oqjc1Jig5J+qzPF3cY1j4oLt/ymlOWo9OplO92JOs65HWq1muyPYAquatrOzm4dudbTaKwUV6UvqbUh16jNnnKu7MuDjlH5DJbHJZRbSjPYXKqf4uQ9sSkv+lZ7KE9Ib9Q=="
+  },
+  {
+    "nome": "aluno 30",
+    "dados": "U2FsdGVkX19ObjombSkrNypoyBCeW+iXXQU0lxFq8LVOUIn9+zdiHKO5Y4CVlUE+17uWzqngfO3Jprzw5sMJ1xm8b9XDaiLzrreu35gYZokG9DTN4kAuYynQPzhrB0eIVg16LA6a203cmNYvOgi82XguxN3NW7TpbSFnWHIz7YVU6vuguxGnEGox46krVASvFHCOFnu+ilOpcgbZzz/oXQ=="
+  }
+];
